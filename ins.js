6f4b46d06b5e338b0a3e9da607977a1e341d3a37
Devansh224 <74537928+Devansh224@users.noreply.github.com>
2021-01-07 09:46:08 +0530

    Add files via upload

---
+++ ins.js
@@ -0,0 +1,3 @@
+function myFunction() {
+alert("You have to click the image of the mole, everytime you click the mole your points will increase. Remember you have only 60 seconds. Click as number of times as you want either 1 or even 100")
+}
